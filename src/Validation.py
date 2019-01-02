b63222801ecc347521b7e39ad011141b8b46f1b3
Arjun Aravind <arjun.aravind1998@gmail.com>
2019-01-02 16:18:02 +0000

    Finish Validate/Exists functions.
    Finished all the Validate/Xxists functions which I can think of.

--- src/Validation.py
+++ src/Validation.py
@@ -1,5 +1,12 @@
 import re
+import MySQLdb
+from SqlConnections import connection
 
+######################
+# VALIDATE FUNCTIONS #
+#####################
+
+# Function which validates if a number is valid or not.
 def validate_number( number ):
 	valid = re.match("[0-9]{10}", number)
 	
@@ -8,6 +15,7 @@ def validate_number( number ):
 	else:
 		return False
 
+# Function which validates if a username is valid or not.
 def validate_username( username ):
 	valid = re.match("^[a-zA-Z0-9]+$", username)
 
@@ -16,6 +24,7 @@ def validate_username( username ):
 	else:
 		return False
 
+# Function which validates if an email is valid or not.
 def validate_email( email ):
 	valid = re.match("[a-zA-Z0-9._]+@[a-zA-Z0-9]+.[a-zA-Z]+", email)
 
@@ -23,3 +32,48 @@ def validate_email( email ):
 		return True
 	else:
 		return False
+
+###################
+# EXISTS FUNCTION #
+###################
+
+
+# Function which checks if a number already exists in the db.
+def number_exists( number ):
+	conn = connection()
+	cursor = conn.cursor()
+
+	sql_query = " SELECT MOBILE FROM CONTACTS WHERE MOBILE='{NUMBER}'; ".format( NUMBER=number )
+	
+	try:
+		cursor.execute(sql_query)
+		conn.close()
+	except:
+		conn.close()
+	
+
+	for MOBILE in cursor.fetchall():
+		if MOBILE[0] == number:
+			return True
+
+	return False
+
+
+# Function which checks if an email already exists in the db.
+def email_exists( email ):
+	conn = connection()
+	cursor = con..cursor()
+
+	sql_query = " SELECT EMAIL FROM CONTACTS WHERE EMAIL='{EMAIL}'; ".format( EMAIL=email )
+
+	try:
+		cursor.execute(sql_query)
+		conn.close()
+	except:
+		conn.close()
+	
+	for EMAIL in cursor.fetchall():
+		if EMAIL[0] == email:
+			return True
+	
+	return False
