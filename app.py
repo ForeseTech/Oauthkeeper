7d5f5f63d1335d9d9bf9106d4edd7fe487a438e4
Arjun Aravind <arjun.aravind1998@gmail.com>
2019-01-04 06:37:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,17 @@ app = Flask(__name__)
 def GetLogin():
 	return render_template('user-login.html')
 
+@app.route('/')
+def GetAdminLogin():
+	return render_template('admin-login.html')
+
 @app.route('/user-login', methods = ['POST'])
 def ValidateUserLogin():
 	if request.method == 'POST':
 		username = request.form['username']
 		password = request.form['password']
 
-		return "{a} and {b}".format(a=username, b=password)
+
 
 @app.route('/admin-login', methods = ['POST'])
 def ValidateAdminLogin():
