5b9d35839c6a8c8dedbd3bc29d8d29fcf428eb6f
Arjun Aravind <arjun.aravind1998@gmail.com>
2019-02-01 16:16:42 +0000

    Fix bugs in Logger module.
    The spelling of the Logger file was changed but it was not in the actual Logger.py files.

--- utilities/logger/Logger.py
+++ utilities/logger/Logger.py
@@ -3,7 +3,7 @@ import datetime
 # Function which logs when a user logs in.
 def logged_in(username):
 	
-	file_object = open("utilities/Logger/OauthkeeperLogs.txt", "a")
+	file_object = open("utilities/logger/OauthkeeperLogs.txt", "a")
 	
 	current_date = datetime.datetime.now()
 	log_message = "[{DATETIME}] {USERNAME} logged in.\n".format( DATETIME=current_date, USERNAME=username )
@@ -13,7 +13,7 @@ def logged_in(username):
 # Function which logs when a user adds a contact.
 def added_contact(username, hr_name, company):
 
-	file_object = open("utilities/Logger/OauthkeeperLogs.txt", "a")
+	file_object = open("utilities/logger/OauthkeeperLogs.txt", "a")
 
 	current_date = datetime.datetime.now()
 	log_message = "[{DATETIME}] {USERNAME} added {HR_NAME} ({COMPANY}) as a contact.\n".format( DATETIME=current_date, USERNAME=username, HR_NAME=hr_name, COMPANY=company )
@@ -23,7 +23,7 @@ def added_contact(username, hr_name, company):
 # Function which logs when a user updates a contact.
 def update_contact(username, hr_name, company):
 
-	file_object = open("utilities/Logger/OauthkeeperLogs.txt", "a")
+	file_object = open("utilities/logger/OauthkeeperLogs.txt", "a")
 
 	current_date = datetime.datetime.now()
 	log_message = "[{DATETIME}] {USERNAME} updated {HR_NAME} ({COMPANY}) in his/her contacts.\n".format( DATETIME=current_date, USERNAME=username, HR_NAME=hr_name, COMPANY=company )
@@ -33,7 +33,7 @@ def update_contact(username, hr_name, company):
 # Function which logs when an admin edits permissions.
 def edited_permissions(username, userid, permissions):
 
-	file_object = open("utilities/Logger/OauthkeeperLogs.txt", "a")
+	file_object = open("utilities/logger/OauthkeeperLogs.txt", "a")
 
 	current_date = datetime.datetime.now()
 	log_message = "[{DATETIME}] {USERNAME} edited the permissions for CONTACT NO {USERID} to '{PERMISSIONS}'.\n".format( DATETIME=current_date, USERNAME=username, USERID=userid, PERMISSIONS=permissions )
